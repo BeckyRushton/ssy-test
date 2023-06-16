69ca9cf9e3ec859702858d07bedc28f07b08c6d5
Becky Rushton <beckyrushton30@gmail.com>
2023-06-16 10:19:03 +0100

    added styled button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Button.scss";
+
+const Button = ({ buttonText }) => {
+  return <div className="button">{buttonText}</div>;
+};
+
+export default Button;
--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -4,12 +4,14 @@ import Table from "../../components/Table/Table";
 import InputBox from "../../components/InputBox/InputBox";
 import Chart from "../../components/Chart/Chart";
 import DailyFeed from "../../components/DailyFeed/DailyFeed";
+import Button from "../../components/Button/Button";
 
 const Main = () => {
   return (
     <div className="main-section">
       <DailyFeed />
       <InputBox />
+      <Button buttonText={"SUBMIT"} />
       <Table />
       <Chart />
     </div>
